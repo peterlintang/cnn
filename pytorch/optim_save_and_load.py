5c8d11f04dade92149315e981fd769d0836b1879
linxiaojie <linxj@pcbaaa.com>
2026-08-17 11:05:41 +0800

    save and load model

--- pytorch/optim_save_and_load.py
+++ pytorch/optim_save_and_load.py
@@ -14,3 +14,25 @@ model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
 start_epoch = checkpoint['epoch'] + 1
+
+
+#################################################################
+# 保存检查点
+checkpoint = {
+    'epoch': epoch,
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+    'loss': loss,
+    # 可以添加其他需要保存的信息
+}
+
+torch.save(checkpoint, 'checkpoint.pth')
+
+# 加载检查点
+checkpoint = torch.load('checkpoint.pth')
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+epoch = checkpoint['epoch']
+loss = checkpoint['loss']
+
+model.eval()  # 或者 model.train() 取决于你的需求
